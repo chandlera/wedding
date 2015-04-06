57b276a856eae37003ac415d7f2377413e881cc7
Alan Chandler <alan.chandler@intrepidlearning.com>
2015-04-06 10:27:17 -0700

    Add Procfile

--- index.js
+++ index.js
@@ -7,42 +7,52 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
+var throng = require('throng');
 
 var routes = require('./routes');
 var app = express();
 
-app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
-app.set('view engine', 'hbs');
-app.set('port', process.env.PORT || 7001);
-app.use(express.static(__dirname + '/public'));
-//app.use(favicon());
-app.use(morgan('combined'));
+var WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-if (process.env.NODE_ENV === 'development') {
-  // only use in development 
-  app.use(errorhandler())
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity
+});
+
+function start() {
+	app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+	app.set('view engine', 'hbs');
+	app.set('port', process.env.PORT || 7001);
+	app.use(express.static(__dirname + '/public'));
+	//app.use(favicon());
+	app.use(morgan('combined'));
+
+	if (process.env.NODE_ENV === 'development') {
+	  // only use in development 
+	  app.use(errorhandler())
+	}
+
+	//new compressor.minify({
+	//    type: 'gcc',
+	//    fileIn: 'public/js/**/*.js',
+	//    fileOut: 'public/js-dist/wildcards-match-gcc.js'
+	//});
+
+	// Using Clean-css for CSS 
+	new compressor.minify({
+	    type: 'clean-css',
+	    fileIn: 'publics/css/**/**.css',
+	    fileOut: 'public/css/base-min-cleancss.css'
+	});
+
+	app.get('/', routes.index);
+	app.get('/venue', routes.venue);
+	app.get('/events', routes.weddingEvents);
+	app.get('/registry', routes.registry);
+	app.get('/engagement', routes.engagement);
+
+	http.createServer(app).listen(app.get('port'), function() {
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
 }
-
-//new compressor.minify({
-//    type: 'gcc',
-//    fileIn: 'public/js/**/*.js',
-//    fileOut: 'public/js-dist/wildcards-match-gcc.js'
-//});
-
-// Using Clean-css for CSS 
-new compressor.minify({
-    type: 'clean-css',
-    fileIn: 'publics/css/**/**.css',
-    fileOut: 'public/css/base-min-cleancss.css'
-});
-
-app.get('/', routes.index);
-app.get('/venue', routes.venue);
-app.get('/events', routes.weddingEvents);
-app.get('/registry', routes.registry);
-app.get('/engagement', routes.engagement);
-
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
 
