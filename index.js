693438091afc4447b93ffeb1f7d436c3e72f2390
Alan Chandler <alan.chandler@intrepidlearning.com>
2015-04-20 14:45:18 -0700

    Test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var http = require('http');
 var expressHbs = require('express3-handlebars');
-// var compressor = require('node-minify');
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
 var throng = require('throng');
-var browserSync = require('browser-sync');
+// var browserSync = require('browser-sync');
 
 var routes = require('./routes');
 var app = express();
@@ -25,26 +24,12 @@ function start() {
 	app.set('view engine', 'hbs');
 	app.set('port', process.env.PORT || 7001);
 	app.use(express.static(__dirname + '/public'));
-	//app.use(favicon());
 	app.use(morgan('combined'));
 
 	if (process.env.NODE_ENV === 'development') {
 	  // only use in development 
 	  app.use(errorhandler())
 	}
-
-	//new compressor.minify({
-	//    type: 'gcc',
-	//    fileIn: 'public/js/**/*.js',
-	//    fileOut: 'public/js-dist/wildcards-match-gcc.js'
-	//});
-
-	// Using Clean-css for CSS 
-	// new compressor.minify({
-	//     type: 'clean-css',
-	//     fileIn: 'publics/css/**/**.css',
-	//     fileOut: 'public/css/base-min-cleancss.css'
-	// });
 
 	app.get('/', routes.index);
 	app.get('/venue', routes.venue);
@@ -54,10 +39,10 @@ function start() {
 
 	http.createServer(app).listen(app.get('port'), function() {
 		console.log('Express server listening on port ' + app.get('port'));
-		browserSync({
-			proxy: 'localhost:' + app.get('port'),
-			files: ['public/**/*.{js,css}']
-		});
+		// browserSync({
+		// 	proxy: 'localhost:' + app.get('port'),
+		// 	files: ['public/**/*.{js,css}']
+		// });
 	});
 }
 
